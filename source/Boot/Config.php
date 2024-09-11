22948cb632f746f1cf54ba7a15daf7f3313ddebf
robertoDorado <robertodorado7@gmail.com>
2024-09-11 16:13:56 -0300

    .gitignore atualizado

---
+++ source/Boot/Config.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Main Setup
+ */
+define("CONF_URL_BASE", "https://localhost");
+define("CONF_URL_TEST", "https://localhost");
+define("CONF_SITE_NAME", "Sistema Simplificado de Pagamentos");
+define("CONF_MAIL", "robertodorado7@gmail.com");
+
+/**
+ * View Setup
+ * Todas configurações relacionadas com a View da aplicação
+ */
+define("CONF_VIEW_PATH", __DIR__ . "/../../themes");
+define("CONF_VIEW_EXT", "php");
+define("CONF_VIEW_THEME", "html");
+
+/**
+ * Database Setup
+ */
+define("CONF_DB_HOST", "db-framework-php");
+define("CONF_DB_USER", "root");
+define("CONF_DB_PASS", "root");
+define("CONF_DB_NAME", "sistema_simplificado_pagamentos");
